c6133d8d8643f74a3ccd4c42b3bb2e658e7e97cd
soulwax <soulwax-95@protonmail.com>
2024-02-06 21:49:45 +0100

    Enhanced PDF generation in DDB script v0.3
    Refactored the DDB book downloader script for increased robustness and maintainability. The update improves content loading by storing fetched pages in an indexed object instead of local storage, which eliminates the need to clear storage on every run and prevents data from previous sessions affecting the current output. Fetching of page content is now a separate function for cleaner execution and error handling. The compile step is deferred until all pages are fetched, leveraging jQuery's `when.apply` to synchronize asynchronous calls. The button text updates to reflect completion state, preventing re-trigger until the page is refreshed. These changes aim to enhance the user experience with a more reliable and understandable PDF generation process.

--- ddb-script.js
+++ ddb-script.js
@@ -1,83 +1,77 @@
-/* globals jQuery, $, waitForKeyElements */
+/* globals jQuery, $ */
 // ==UserScript==
-// @name         DDB Book Downloader
+// @name         DDB Book Downloader Enhanced
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Save your DBB books to PDF!
-// @author       C T Zaran (and shanix)
+// @version      0.3
+// @description  Save your DDB books to PDF with improved content loading!
+// @author       Enhanced by Code Wizard
 // @match        https://www.dndbeyond.com/sources/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dndbeyond.com
 // @require      https://code.jquery.com/jquery-3.6.3.min.js
 // @grant        none
 // ==/UserScript==
 
-$().ready(function () {
-  localStorage.clear(); // We need to clear the localStorage, else our PDF is going to be fubar.
+(function () {
+  "use strict";
 
-  // Reworked how we get pages.  First we get all the 'compendiums' (i.e. each of the blocks of links),
-  // then we iterate over all the anchors that don't include a # (which indicates a header, not the whole page),
-  // and save those to an array.  Should get everything.  Hopefully.  Theoretically.  Probably.
-  let book_title = $(document).attr("title");
-  let raw_compendiums = $(".compendium-toc-full-text");
-  let compendium_pages = [];
-  raw_compendiums.each(function (i, obj) {
-    $(obj)
-      .find("a")
-      .each(function () {
-        if (!$(this).attr("href").includes("#")) {
-          compendium_pages.push($(this).attr("href"));
-        }
+  $(document).ready(function () {
+    let bookTitle = document.title;
+    let contentStorage = {};
+    let contentOrder = [];
+
+    function fetchPageContent(url, index) {
+      return $.get(url, function (data) {
+        let pageContent = $("<div>")
+          .append($.parseHTML(data))
+          .find(".p-article-content")
+          .html();
+        contentStorage[index] = pageContent;
       });
-  });
-
-  let first_compendium = raw_compendiums[0];
-  let new_book_contents =
-    '<div><button class="doPDF" type="button">Do PDF</button></div>' +
-    $(first_compendium).html(); // Add a button before our original HTML
-  $(first_compendium).html(new_book_contents); // Restore our book contents
-
-  let last_page = 0; // Default for our last page variable
-  $(compendium_pages).each(function (i, v) {
-    //Cycle all our pages, we want to know what page number we are (starting at 0, becuase arrays start at 0).
-    $.get(v, function (data) {
-      //Here, we are getting the page data from the URL for each page of our book.
-      let pageData = $(data).find(".p-article-content").html(); //Grab just the page contents we care about
-      localStorage.setItem(i, pageData); //Now we save the HTML for our page to our local storage for later
-    });
-    last_page = i; //Set last page to be our i (index) number for later. Will always be the last page once this loop is completed.
-  });
-
-  $(".doPDF").on("click", function () {
-    // Lets start building our book!
-
-    let open_book = window.open();
-    // We need to hijack the DBB CSS or else our book looks bad.
-    let book_HTML =
-      "<!DOCTYPE html>" +
-      '<html lang="en-us" class="no-js">' +
-      '<meta charset="UTF-8">' +
-      "<title>" +
-      book_title +
-      "</title>" +
-      '<link rel="stylesheet" href="https://www.dndbeyond.com/content/1-0-2352-0/skins/blocks/css/compiled.css"/>' +
-      '<link rel="stylesheet" href="https://www.dndbeyond.com/content/1-0-2352-0/skins/waterdeep/css/compiled.css"/>' +
-      '<link rel="stylesheet" type="text/css" href="https://www.dndbeyond.com/api/custom-css" />' +
-      "<style>body {width: 850px; margin-left:30px}</style>";
-
-    // Lets loop our pages, while our current page is less than or equal to our last page, grab that previously saved data and add it to our book
-    let current_page = 0; // Default current page for later.
-    while (current_page <= last_page) {
-      book_HTML += localStorage.getItem(current_page);
-      book_HTML +=
-        '<div style = "display:block; clear:both; page-break-after:always;"></div>';
-      current_page += 1; // Bump current page number up by 1.
     }
 
-    book_HTML = book_HTML + "</html>"; //Close our our HTML tag or browsers will get funny about it.
+    function compileBook() {
+      let compiledContent = contentOrder
+        .map((index) => contentStorage[index])
+        .join('<div style="page-break-after: always;"></div>');
+      return (
+        "<!DOCTYPE html>" +
+        '<html lang="en"><head><meta charset="UTF-8"><title>' +
+        bookTitle +
+        "</title>" +
+        '<link rel="stylesheet" href="https://www.dndbeyond.com/content/1-0-2352-0/skins/blocks/css/compiled.css"/>' +
+        '<link rel="stylesheet" href="https://www.dndbeyond.com/content/1-0-2352-0/skins/waterdeep/css/compiled.css"/>' +
+        '<link rel="stylesheet" type="text/css" href="https://www.dndbeyond.com/api/custom-css" />' +
+        "<style>body {width: 850px; margin-left:30px;}</style>" +
+        "</head><body>" +
+        compiledContent +
+        "</body></html>"
+      );
+    }
 
-    open_book.document.write(book_HTML); //Open tab with our Book!
-    $(this)
-      .attr("disabled", true)
-      .text("PDF Done, refresh page to generate again"); //Disable the button, just prevents oddites from occuring with running this more than once per refresh.
+    $(".compendium-toc-full-text a")
+      .not('[href*="#"]')
+      .each(function (index) {
+        let href = $(this).attr("href");
+        contentOrder.push(index);
+        fetchPageContent(href, index);
+      });
+
+    $('<button id="compilePDF" type="button">Compile PDF</button>')
+      .insertBefore(".compendium-toc-full-text")
+      .on("click", function () {
+        $.when
+          .apply(
+            $,
+            Object.keys(contentStorage).map((key) => contentStorage[key])
+          )
+          .then(function () {
+            let bookContent = compileBook();
+            let newWindow = window.open();
+            newWindow.document.open();
+            newWindow.document.write(bookContent);
+            newWindow.document.close();
+            $("#compilePDF").prop("disabled", true).text("PDF Compiled");
+          });
+      });
   });
-});
+})();
